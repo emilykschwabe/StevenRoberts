734ed7e896c2af114a2eb2cf4fed45836d221974
sr320 <roberts.sbr@gmail.com>
2019-11-05 10:25:14 -0800

    added comment describing length_cat column

--- scripts/Lecture-notes.R
+++ scripts/Lecture-notes.R
@@ -12,6 +12,9 @@ library(dplyr)
 
 
 
+
+# this code adds a column called length_cat, and if length is > 300, big is added in column,  if not small is.
+
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
 

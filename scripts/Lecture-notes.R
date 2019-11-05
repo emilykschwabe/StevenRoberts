c274fee7290c2d65b3a54e3486a63c2520e882c4
sr320 <roberts.sbr@gmail.com>
2019-11-05 10:49:19 -0800

    made plot length X  scalelength

--- scripts/Lecture-notes.R
+++ scripts/Lecture-notes.R
@@ -32,3 +32,17 @@ fish_data_cat = fish_data %>%
 fish_data_cat_clean <- filter(fish_data_cat, scalelength  > 1)
 
 
+
+
+#Question 5 
+# To look at the relationship between the length of each fish’s body and the size of its scale across the different lakes sampled in these data, create a scatterplot with length on the x-axis and scalelength on the y-axis, then color the points using lakeid.
+
+library(ggplot2)
+
+
+
+
+
+ggplot(fish_data_cat_clean, aes(x = length, y = scalelength, color = lakeid)) + 
+  geom_point(size=3) 
+
